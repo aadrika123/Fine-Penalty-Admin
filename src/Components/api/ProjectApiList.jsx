aa7e106b00ad21f646adc19257b614bdef238f73
Talib Hussain <talibplay111@gmail.com>
2023-12-14 15:42:45 +0530

    http added

--- src/Components/api/ProjectApiList.jsx
+++ src/Components/api/ProjectApiList.jsx
@@ -4,157 +4,157 @@ export default function ProjectApiList() {
     let baseUrl = BackendUrl
     let apiList = {
 
-        api_moduleList: `${baseUrl}/api/user-managment/v2/crud/module/list`,
+        api_moduleList: `${baseUrl}/user-managment/v2/crud/module/list`,
         
         //heartBeatApi
-        api_checkHeartBeat: `${baseUrl}/api/heartbeat`,
+        api_checkHeartBeat: `${baseUrl}/heartbeat`,
         // 1 API TO MENU LIST
-        api_getFreeMenuList: `${baseUrl}/api/menu/by-module`,
+        api_getFreeMenuList: `${baseUrl}/menu/by-module`,
 
-        api_login: `${baseUrl}/api/login`,
-        api_logout: `${baseUrl}/api/logout`,
-        api_setPassword: `${baseUrl}/api/user/set-password`,
+        api_login: `${baseUrl}/login`,
+        api_logout: `${baseUrl}/logout`,
+        api_setPassword: `${baseUrl}/user/set-password`,
     
        
         // 19 API TO GET WORKFLOW BASIC INFO LIKE PERMISSIONS/WORKFLOW-CANDIDATES
-        api_workflowInfo: `${baseUrl}/api/workflow/role-map/workflow-info`,
+        api_workflowInfo: `${baseUrl}/workflow/role-map/workflow-info`,
 
 
         // 21 API TO POST DEPARTMENTAL COMMUNICATION DATA
-        api_postDepartmental: `${baseUrl}/api/post-custom-data`,
+        api_postDepartmental: `${baseUrl}/post-custom-data`,
 
         // 22 API TO TO GET SAF DEPARTMENTAL COMMUNICATION LIST
-        api_getDepartmentalData: `${baseUrl}/api/get-all-custom-tab-data`,
+        api_getDepartmentalData: `${baseUrl}/get-all-custom-tab-data`,
 
         
         //application demand detail in demand screen
-        api_verifyDocuments: `${baseUrl}/api/workflows/document/verify-reject`,
+        api_verifyDocuments: `${baseUrl}/workflows/document/verify-reject`,
         //application demand detail in demand screen
-        api_changePassword: `${baseUrl}/api/change-password`,
+        api_changePassword: `${baseUrl}/change-password`,
 
         // API TO EDIT ADMIN PROFILE
-        api_editAdminProfile: `${baseUrl}/api/edit-my-profile`,
+        api_editAdminProfile: `${baseUrl}/edit-my-profile`,
         // API TO FETCH JSK DASHBOARD RECENT APPLICATIONS AND RECENT PAYMENTS
 
         // API TO FETCH NOTIFICATION DATA
-        api_getNotification: `${baseUrl}/api/get-user-notifications`,
+        api_getNotification: `${baseUrl}/get-user-notifications`,
         // API TO CREATE NOTIFICATION DATA
-        api_createNotification: `${baseUrl}/api/dashboard/jsk/prop-dashboard`,
+        api_createNotification: `${baseUrl}/dashboard/jsk/prop-dashboard`,
         // API TO DELETE NOTIFICATION DATA
-        api_deleteNotification: `${baseUrl}/api/dashboard/jsk/prop-dashboard`,
+        api_deleteNotification: `${baseUrl}/dashboard/jsk/prop-dashboard`,
 
         // ════════════════════════║🔰 Marriage Api List 🔰║═══════════════════════════ 
-        marriageInbox : baseUrl + '/api/marriage/inbox',
-        marriageDetails : baseUrl + '/api/marriage/details',
-        workflowInfo : baseUrl + "/api/workflow/role-map/workflow-info",
-        appointSet : baseUrl + "/api/marriage/set-appiontment-date",
-        getUploadedDocument : baseUrl + "/api/marriage/get-uploaded-document",
-        docVerify : baseUrl + "/api/marriage/doc-verify-reject",
-        approveReject : baseUrl + "/api/marriage/final-approval-rejection",
-        approvedList : baseUrl + "/api/marriage/approved-application",
-        marriageApplicationList : baseUrl + "/api/marriage/search-application",
-        marriageOrderId : baseUrl + "/api/marriage/generate-order-id",
-        api_postMarriageOfflinePayment: baseUrl + '/api/marriage/offline-payment',
-        api_MarriageReceipt: baseUrl + '/api/marriage/payment-receipt',
-        api_marriageNextLevel: baseUrl + '/api/marriage/post-next-level',
+        marriageInbox : baseUrl + '/marriage/inbox',
+        marriageDetails : baseUrl + '/marriage/details',
+        workflowInfo : baseUrl + "/workflow/role-map/workflow-info",
+        appointSet : baseUrl + "/marriage/set-appiontment-date",
+        getUploadedDocument : baseUrl + "/marriage/get-uploaded-document",
+        docVerify : baseUrl + "/marriage/doc-verify-reject",
+        approveReject : baseUrl + "/marriage/final-approval-rejection",
+        approvedList : baseUrl + "/marriage/approved-application",
+        marriageApplicationList : baseUrl + "/marriage/search-application",
+        marriageOrderId : baseUrl + "/marriage/generate-order-id",
+        api_postMarriageOfflinePayment: baseUrl + '/marriage/offline-payment',
+        api_MarriageReceipt: baseUrl + '/marriage/payment-receipt',
+        api_marriageNextLevel: baseUrl + '/marriage/post-next-level',
         
-        api_postMarriageSubmission: `${baseUrl}/api/marriage/apply`,
-        api_getDocList: `${baseUrl}/api/marriage/get-doc-list`,
-        api_docUpload: `${baseUrl}/api/marriage/upload-document`,
-        api_getDetails: `${baseUrl}/api/marriage/static-details`,
-        api_getList: `${baseUrl}/api/marriage/applied-application`,
-        api_deleteApplication: `${baseUrl}/api/marriage/`,
-        api_editMarriageApplication: `${baseUrl}/api/marriage/edit-application`,
+        api_postMarriageSubmission: `${baseUrl}/marriage/apply`,
+        api_getDocList: `${baseUrl}/marriage/get-doc-list`,
+        api_docUpload: `${baseUrl}/marriage/upload-document`,
+        api_getDetails: `${baseUrl}/marriage/static-details`,
+        api_getList: `${baseUrl}/marriage/applied-application`,
+        api_deleteApplication: `${baseUrl}/marriage/`,
+        api_editMarriageApplication: `${baseUrl}/marriage/edit-application`,
 
         // 👉 ================ Fines & Penalties API List =================== 👈
-        api_submitInfractionForm : `${baseUrl}/api/penalty-record/crud/save`,
-        api_updateInfractionForm : `${baseUrl}/api/penalty-record/crud/edit`,
-        api_getInfractionById    : `${baseUrl}/api/v2/penalty-record/crud/show`,
-        api_getInfractionList     : `${baseUrl}/api/penalty-record/crud/active-all`,        
+        api_submitInfractionForm : `${baseUrl}/penalty-record/crud/save`,
+        api_updateInfractionForm : `${baseUrl}/penalty-record/crud/edit`,
+        api_getInfractionById    : `${baseUrl}/v2/penalty-record/crud/show`,
+        api_getInfractionList     : `${baseUrl}/penalty-record/crud/active-all`,        
         
-        api_getViolationList     : `${baseUrl}/api/violation/crud/list`,        
-        api_getWardList          : `${baseUrl}/api/ward-list`,        
+        api_getViolationList     : `${baseUrl}/violation/crud/list`,        
+        api_getWardList          : `${baseUrl}/ward-list`,        
         
-        api_violationMasterList : `${baseUrl}/api/violation/crud/list`,  
-        api_getViolationById     : `${baseUrl}/api/violation/crud/get`,
-        api_updateViolation     : `${baseUrl}/api/violation/crud/edit`,
-        api_addViolation        : `${baseUrl}/api/violation/crud/save`,
-        api_deleteViolation     : `${baseUrl}/api/violation/crud/delete`,
+        api_violationMasterList : `${baseUrl}/violation/crud/list`,  
+        api_getViolationById     : `${baseUrl}/violation/crud/get`,
+        api_updateViolation     : `${baseUrl}/violation/crud/edit`,
+        api_addViolation        : `${baseUrl}/violation/crud/save`,
+        api_deleteViolation     : `${baseUrl}/violation/crud/delete`,
 
-        fpInbox : baseUrl + '/api/penalty-record/inbox',
-        fpDetails : baseUrl + '/api/penalty-record/detail',
-        fpDocList : baseUrl + '/api/penalty-record/crud/show-document',
-        fpApprove : baseUrl + '/api/penalty-record/approve',
-        api_fpChallan2 : baseUrl + '/api/penalty-record/get-challan',
-        getFpUploadedDocument : baseUrl + "/api/penalty-record/get-uploaded-document",
+        fpInbox : baseUrl + '/penalty-record/inbox',
+        fpDetails : baseUrl + '/penalty-record/detail',
+        fpDocList : baseUrl + '/penalty-record/crud/show-document',
+        fpApprove : baseUrl + '/penalty-record/approve',
+        api_fpChallan2 : baseUrl + '/penalty-record/get-challan',
+        getFpUploadedDocument : baseUrl + "/penalty-record/get-uploaded-document",
 
-        api_FPTrack: `${baseUrl}/api/penalty-record/challan-search`,
+        api_FPTrack: `${baseUrl}/penalty-record/challan-search`,
 
-        api_FpApplyReport:           `${baseUrl}/api/penalty-record/challan-search`,
-        api_ChallanGeneratingReport: `${baseUrl}/api/report/challan-wise`,
-        api_ViolationWiseReport:     `${baseUrl}/api/report/violation-wise`,
-        api_CollectionReport:        `${baseUrl}/api/report/collection-wise`,
+        api_FpApplyReport:           `${baseUrl}/penalty-record/challan-search`,
+        api_ChallanGeneratingReport: `${baseUrl}/report/challan-wise`,
+        api_ViolationWiseReport:     `${baseUrl}/report/violation-wise`,
+        api_CollectionReport:        `${baseUrl}/report/collection-wise`,
 
-        api_getChallanById:        `${baseUrl}/api/penalty-record/get-challan`,
-        api_challanOfflinePayment: `${baseUrl}/api/penalty-record/offline-challan-payment`,
+        api_getChallanById:        `${baseUrl}/penalty-record/get-challan`,
+        api_challanOfflinePayment: `${baseUrl}/penalty-record/offline-challan-payment`,
 
-        api_FpReceipt : baseUrl + '/api/penalty-record/payment-receipt',
+        api_FpReceipt : baseUrl + '/penalty-record/payment-receipt',
 
-        api_getViolationList:    `${baseUrl}/api/violation/list`,
-        api_getSectionList:      `${baseUrl}/api/section/list`,
-        api_assignViolation:     `${baseUrl}/api/violation/onspot`,
-        api_getDepartmentList:   `${baseUrl}/api/department/list`,
-        api_getUserList:         `${baseUrl}/api/user-list`,
+        api_getViolationList:    `${baseUrl}/violation/list`,
+        api_getSectionList:      `${baseUrl}/section/list`,
+        api_assignViolation:     `${baseUrl}/violation/onspot`,
+        api_getDepartmentList:   `${baseUrl}/department/list`,
+        api_getUserList:         `${baseUrl}/user-list`,
         
-        api_getViolationByDept:  `${baseUrl}/api/violation/by-department`,
+        api_getViolationByDept:  `${baseUrl}/violation/by-department`,
 
-        api_listDepartment: `${baseUrl}/api/department/crud/list`,
-        api_addDepartment: `${baseUrl}/api/department/crud/save`,
+        api_listDepartment: `${baseUrl}/department/crud/list`,
+        api_addDepartment: `${baseUrl}/department/crud/save`,
         api_updateDepartment: `${baseUrl}/`,
-        api_deleteDepartment: `${baseUrl}/api/department/crud/delete`,
+        api_deleteDepartment: `${baseUrl}/department/crud/delete`,
 
-        api_listSection: `${baseUrl}/api/section/crud/list`,
-        api_addSection: `${baseUrl}/api/section/crud/save`,
+        api_listSection: `${baseUrl}/section/crud/list`,
+        api_addSection: `${baseUrl}/section/crud/save`,
         api_updateSection: `${baseUrl}/`,
-        api_deleteSection: `${baseUrl}/api/section/crud/delete`,
+        api_deleteSection: `${baseUrl}/section/crud/delete`,
         
-        api_compData: `${baseUrl}/api/report/comparison`,
+        api_compData: `${baseUrl}/report/comparison`,
 
-        api_addRole:        `${baseUrl}/api/wfrole/crud/save`,
-        api_updateRole:     `${baseUrl}/api/wfrole/crud/edit`,
-        api_deletedRole:    `${baseUrl}/api/wfrole/crud/delete`,
-        api_listRole:       `${baseUrl}/api/wfrole/crud/list`,
+        api_addRole:        `${baseUrl}/wfrole/crud/save`,
+        api_updateRole:     `${baseUrl}/wfrole/crud/edit`,
+        api_deletedRole:    `${baseUrl}/wfrole/crud/delete`,
+        api_listRole:       `${baseUrl}/wfrole/crud/list`,
 
-        api_addUser:        `${baseUrl}/api/user/crud/create`,
-        api_updateUser:     `${baseUrl}/api/user/crud/edit`,
-        api_deletedUser:    `${baseUrl}/api/user/crud/delete`,
-        api_listUser:       `${baseUrl}/api/user/crud/list`,
+        api_addUser:        `${baseUrl}/user/crud/create`,
+        api_updateUser:     `${baseUrl}/user/crud/edit`,
+        api_deletedUser:    `${baseUrl}/user/crud/delete`,
+        api_listUser:       `${baseUrl}/user/crud/list`,
 
-        api_assignRole:     `${baseUrl}/api/user/role-assign`,
+        api_assignRole:     `${baseUrl}/user/role-assign`,
 
         // Cash Verification
-        api_cashVerificaionList : `${baseUrl}/api/fines/cash-verification-list`,
-        api_cashVerificaionById : `${baseUrl}/api/fines/cash-verification-dtl`,
-        api_verifyCash : `${baseUrl}/api/fines/verify-cash`,
+        api_cashVerificaionList : `${baseUrl}/fines/cash-verification-list`,
+        api_cashVerificaionById : `${baseUrl}/fines/cash-verification-dtl`,
+        api_verifyCash : `${baseUrl}/fines/verify-cash`,
 
         // ════════════════════║ ONLINE PAYMENT APIS  ║═════════════════════════
-        // api_generateOrderId : `${baseUrl}/api/fines/razorpay/initiate-payment`,
-        api_generateOrderId : `${baseUrl}/api/fines/citizen-online-payment`,
+        // api_generateOrderId : `${baseUrl}/fines/razorpay/initiate-payment`,
+        api_generateOrderId : `${baseUrl}/fines/citizen-online-payment`,
         api_verifyPaymentStatus: `${baseUrl}/`, 
 
         // ════════════════════║ API TO SEARCH CHALLAN DIRECT  ║═════════════════════════
-        api_searchChallanDirect : `${baseUrl}/api/penalty-record/citizen-challan-search`,
+        api_searchChallanDirect : `${baseUrl}/penalty-record/citizen-challan-search`,
 
         // ════════════════════║ API TO GET TRANSACTION NUMBER FROM ORDER ID  ║═════════════════════════
-        api_getTransactionNo : `${baseUrl}/api/penalty-record/get-tran-no`,
+        api_getTransactionNo : `${baseUrl}/penalty-record/get-tran-no`,
 
         // ═════════════║ API TO GET SEND ONLINE PAYMENT RESPONSE ║══════════════════ 
-        api_sendOnlineResponse: `${baseUrl}/api/fines/razorpay/save-response`,
+        api_sendOnlineResponse: `${baseUrl}/fines/razorpay/save-response`,
 
     //    -==================Citizen API List=========================
-    api_enf_officer: `${baseUrl}/api/user/enf-officer`,
-    api_enf_cell: `${baseUrl}/api/user/enf-officer`,
-    api_violation_list: `${baseUrl}/api/v2/violation/crud/list`,
+    api_enf_officer: `${baseUrl}/user/enf-officer`,
+    api_enf_cell: `${baseUrl}/user/enf-officer`,
+    api_violation_list: `${baseUrl}/v2/violation/crud/list`,
 
     }
 
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-    server: {
-    port : 5000
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://egov.rsccl.in:8443',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''), // Remove /api prefix
+      },
+    },
+    port: 5000,
   },
   build: {
-    chunkSizeWarningLimit: 5000 // increase the chunk size limit to 1000kb
+    chunkSizeWarningLimit: 5000,
   },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-})
+});
